49aa2483b03d1b2d12670a89dd520156b58c1631
Anton Boiko <bo1ko.ant@yandex.com>
2023-05-29 11:18:38 +0300

    added a default picture and started working with models

--- social_network/urls.py
+++ social_network/urls.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 from django.urls import path
 from my_app import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.mainPage, name="main"),
 ]
+
+
+urlpatterns = urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

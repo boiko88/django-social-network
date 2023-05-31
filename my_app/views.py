3364ab7fda7c68eb7f6080071c384479eef494a2
Anton Boiko <bo1ko.ant@yandex.com>
2023-05-31 19:18:20 +0300

    test of the register and login functionality. Now a new user can be created and the user can login the platform

--- my_app/views.py
+++ my_app/views.py
@@ -47,4 +47,13 @@ def signin(request):
         username = request.POST['username']
         password = request.POST['password']
         
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('main')
+        else:
+            messages.info(request, 'Credentials are wrong')
+            return redirect('signin')
+        
     return render(request, 'signin.html')

30800db970b7c82c12be1ea922da909cf4b388ee
Anton Boiko <bo1ko.ant@yandex.com>
2023-06-01 19:00:01 +0300

    added settings page and started to work on it

--- my_app/views.py
+++ my_app/views.py
@@ -30,6 +30,11 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
                 
+                # Log use in and redirect to settings page
+                
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                
                 # Create a profile object for new users created
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
@@ -67,3 +72,8 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+
+@login_required(login_url='signin')
+def settings(request):
+    return render(request, 'settings.html')

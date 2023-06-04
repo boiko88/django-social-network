9b93cd2d753ae8fd5b129ee3a038daffbd67b562
Anton Boiko <bo1ko.ant@yandex.com>
2023-06-04 20:08:48 +0300

    added post creating functionality. I had to comment number of likes variable in the models in order to let new posts be created

--- my_app/views.py
+++ my_app/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
-from django.contrib.auth.models import auth
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
 
 
 @login_required(login_url='signin')
 def mainPage(request):
-    return render(request, 'main.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    
+    context = {
+        'user_profile': user_profile,
+    }
+    return render(request, 'main.html', context)
 
 
 def signup(request):
@@ -105,3 +110,18 @@ def settings(request):
         
     }
     return render(request, 'settings.html', context)
+
+
+@login_required(login_url='signin')
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+        
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        
+        return redirect('main')
+    else:
+        return redirect('main')

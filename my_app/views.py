6b21f71c194d52444bcd370d0484c9f4c42b6652
Anton Boiko <bo1ko.ant@yandex.com>
2023-06-09 23:47:56 +0300

    finished the project by adding suggestions

--- my_app/views.py
+++ my_app/views.py
@@ -4,13 +4,13 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Profile, Post, LikePost, FollowersCount
 from itertools import chain
+import random
 
 
 @login_required(login_url='signin')
 def main_page(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    posts = Post.objects.all()
     
     user_following_list = []
     feed = []
@@ -26,10 +26,38 @@ def main_page(request):
         
     feed_list = list(chain(*feed))
     
+    # User Suggestions
+    
+    all_users = User.objects.all()
+    user_following_all = []
+    
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+        
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    current_user = User.objects.filter(username=request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if ( x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+    
+    username_profile = []
+    username_profile_list = []
+    
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+        
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+        
+    
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+    
     context = {
         'user_profile': user_profile,
         'posts': feed_list,
-        # It used to be 'posts': feed_list, but now we want users to see only post of the users they follow
+        'suggestions_username_profile_list': suggestions_username_profile_list[:4],
+        # It used to be 'posts': posts, but now we want users to see only post of the users they follow
     }
     return render(request, 'main.html', context)
 
